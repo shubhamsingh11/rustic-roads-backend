73558bce9f51c925090154d048e01f59bab69771
Shubham Singh <shubham11singh1997@gmail.com>
2026-01-03 22:17:26 +0530

    update models

--- src/models/blogs/blog.models.js
+++ src/models/blogs/blog.models.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const postSchema = new mongoose.Schema(
   {
@@ -10,10 +11,12 @@ const postSchema = new mongoose.Schema(
       required: true,
     },
     images: [{ type: String }], // Array of S3 URLs
+    coverImage: { type: String }, // S3 URL
     tags: [{ type: String }], // ["mountains", "himalayas"]
     destination: { type: mongoose.Schema.Types.ObjectId, ref: "Destination" },
     likesCount: { type: Number, default: 0 },
     commentsCount: { type: Number, default: 0 },
+    views: { type: Number, default: 0 },
     isPublished: { type: Boolean, default: false },
   },
   { timestamps: true }
--- src/models/blogs/user.models.js
+++ src/models/blogs/user.models.js
@@ -1,15 +1,32 @@
 // models/User.js
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true, trim: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true },
-    profilePic: { type: String }, // S3 URL
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: { type: String, required: true, trim: true },
+    password: { type: String, required: [true, "Password is required"] },
+    profilePic: { type: String, required: true }, // S3 URL
+    watchHistory: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
     bio: { type: String },
     role: { type: String, enum: ["user", "admin"], default: "user" },
+    refreshToken: { type: String },
   },
   { timestamps: true }
 );
@@ -24,6 +41,29 @@ userSchema.pre("save", async function (next) {
 // Compare password
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN }
+  );
 };
 
 export default mongoose.model("User", userSchema);
